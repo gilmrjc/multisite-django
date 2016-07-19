eb2bc9333cdac09080df6fd3286054387618d772
Gildardo Adrian Maravilla Jacome <gilmrjc@gmail.com>
2016-07-19 04:25:53 -0500

    Added nginx configuration

--- example-apps/app1/example/settings.py
+++ example-apps/app1/example/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
+    'landing',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK" : lambda x: True,
+}
